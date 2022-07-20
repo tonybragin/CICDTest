c23dd284c0e566c654daee088708df81e7591bf8
Брагин Антон Сергеевич <ASBragin1@sberbank.ru>
2022-07-20 14:00:28 +0700

    Init project

---
+++ CICDTest/CICDTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CICDTest
+//
+//  Created by Anton Bragin on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
